3e2a9aa864ede36cf80bc68a9a81ddf33f9f9e12
David Berko <David Berko>
2014-10-02 12:31:30 -0500

    first commit

---
+++ CreateSpace2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreateSpace2::Application.config.secret_token = 'caea06a72d2bef299a53a7ac69c7e6245dbcc839982873ab0f90eecb4bd2033ee8a68b302b9847a598b5ec284f73b352549bd40c561dec5213ec7382111d06e2'
